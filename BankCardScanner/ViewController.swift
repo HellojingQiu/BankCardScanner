5a996f2e7560bd1ea17622e0a432100134be2868
OliHire-HellowJingQiu <hellowjingqiu@CongYi-JokerV.local>
2015-04-28 10:02:48 +0800

    Initial Commit

---
+++ BankCardScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankCardScanner
+//
+//  Created by OliHire-HellowJingQiu on 15/4/28.
+//  Copyright (c) 2015年 OliHire-HellowJingQiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
