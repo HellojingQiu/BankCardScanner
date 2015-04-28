87373e002659ac4750c78c8ccb4104bd7445a12b
OliHire-HellowJingQiu <hellowjingqiu@CongYi-JokerV.local>
2015-04-28 11:52:30 +0800

    first

--- BankCardScanner/ViewController.swift
+++ BankCardScanner/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CardIOPaymentViewControllerDelegate {
+    @IBOutlet weak var labelScanResult: UILabel!
+    @IBOutlet weak var iamgeCard: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func scanBegin(sender: AnyObject) {
+        var cardVC = CardIOPaymentViewController(paymentDelegate: self)
+        cardVC.modalPresentationStyle = UIModalPresentationStyle.FormSheet
+        
+        presentViewController(cardVC, animated: true, completion: nil)
     }
 
+    func userDidCancelPaymentViewController(paymentViewController: CardIOPaymentViewController!) {
+        labelScanResult.text = "用户取消扫描!"
+        paymentViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func userDidProvideCreditCardInfo(cardInfo: CardIOCreditCardInfo!, inPaymentViewController paymentViewController: CardIOPaymentViewController!) {
+        if let info = cardInfo{
+            labelScanResult.text = "卡号:\(info.cardNumber)\n类型:\(info.cardType)\n" +
+            "过期时间:\(info.expiryYear)年 \(info.expiryMonth)月\n" +
+            "CVV校验\(info.cvv)\n"
+            iamgeCard.image = info.cardImage;
+        }
+        paymentViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
